0b67eb82cc0227876a7cdf03d56ddc0dcf202fcc
cr7cr8 <cr7cr8@126.com>
2020-03-08 18:39:06 +0800

    commit

---
+++ client/build/precache-manifest.2597fffa4ac2f489af2591bc08b4e362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed0f32d581bc14d5c07e22f098da908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afc3971d66ee29831cd",
+    "url": "/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "7080589e998101e5a194",
+    "url": "/static/js/2.87019a03.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.87019a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afc3971d66ee29831cd",
+    "url": "/static/js/main.dcc0dbf7.chunk.js"
+  },
+  {
+    "revision": "f6dc96b7b0f854d72d62",
+    "url": "/static/js/runtime-main.0ed715c4.js"
+  }
+]);
